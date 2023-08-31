f0cd8e0bca1c6f5cfe2ff5880bea28403de65261
­Amna2017 <asiaashour2017@gmai.com>
2023-08-31 05:30:02 -0700

    mmm

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "binary_trees.h"
+
+/**
+ *  binary_tree_node - create new nood
+ *  @parent: its parant node
+ *
+ *  @value: its value
+ */
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	if (parent == NULL)
+		return NULL;
+}
